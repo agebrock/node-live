3b32c5bd08bda027dfd16074cb474d6695bcf36a
Christoph Hagenbrock <christoph.hagenbrock@googlemail.com>
2015-08-13 14:03:32 +0200

    Release 0.0.1

--- nlive.js
+++ nlive.js
@@ -1,77 +1,83 @@
 'use strict';
 
 var _ = require('lodash'),
-	bluebird = ('bluebird'),
-	fs = require('fs'),
-	util = require('util'),
-	path = require('path'),
-	repl = require('repl');
+    bluebird = ('bluebird'),
+    fs = require('fs'),
+    util = require('util'),
+    path = require('path'),
+    repl = require('repl'),
+    shelljs = require('shelljs');
 
 var context = {};
-
-
+context.x = shelljs.exec;
 //mapping lodash to "_" seems to make problems with repl
 var scopeMapping = {
-	bluebird:['Promise'],
-	lodash:['lodash','l']
-}
-
+    bluebird: ['Promise'],
+    lodash: ['lodash', 'l']
+};
 
 //_.extend(global, require('./lib/native-modules'));
 var pkg = require('./package.json');
-_.forEach(pkg.dependencies,function(pkgVersion, pkgName){
 
-	var scopeName = pkgName;
-	if(scopeMapping[scopeName]){
-		scopeName = scopeMapping[scopeName];
-	}
+_.forEach(pkg.dependencies, function(pkgVersion, pkgName) {
 
-	var m = {};
-	if(Array.isArray(scopeName)){
-		scopeName.forEach(function(scopeName){
-            console.log('Loading: %s into %s',pkgName, scopeName);
-			m[scopeName] = require(pkgName);
-		});
-	}else{
+    var scopeName = pkgName;
+
+    if (scopeMapping[scopeName]) {
+        scopeName = scopeMapping[scopeName];
+    }
+
+    var m = {};
+
+    if (Array.isArray(scopeName)) {
+        scopeName.forEach(function(scopeName) {
+            console.log('Loading: %s into %s', pkgName, scopeName);
+            m[scopeName] = require(pkgName);
+        });
+    } else {
         scopeName = _.camelCase(scopeName);
-        console.log('Loading: %s into %s',pkgName, scopeName);
-		m[scopeName] = require(pkgName);
-	}
+        console.log('Loading: %s into %s', pkgName, scopeName);
+        m[scopeName] = require(pkgName);
+    }
 
-	_.extend(context, m);
+    _.extend(context, m);
 });
 var values = {};
-try{
+
+try {
     values = require('./scope');
-}catch(e){
+} catch (e) {
     console.log('WARN: Scope not loaded ! .. using empty one')
 }
 
 _.extend(global, context, values);
 
 console.log('Your extended scope:');
-console.log(util.inspect(values,{colors:true,depth:null, showHidden:false}));
+console.log(util.inspect(values, {colors: true, depth: null, showHidden: false}));
 
-
-var replInstance = repl.start({useGlobal:true});
+var replInstance = repl.start({useGlobal: true});
 var context = replInstance.context;
-
-
+context.replInstance = replInstance;
 //mod repl to implement custom comamnds
 var originalEval = replInstance.eval;
+
 function run(cmd, context, filename, callback) {
-    if(cmd.toString().trim('\n') == 'run'){
-        try{
+    var args = arguments;
+    cmd = cmd.toString().trim('\n');
+    if (cmd == 'run') {
+        try {
             context.cwd = require(process.cwd());
             console.log('package loaded into "cwd"')
-        }catch(e){
+        } catch (e) {
             console.log('failed to load local context')
         }
         return true;
     }
-  return originalEval.apply(this, arguments);
+    var run = false;
+
+    originalEval.apply(this, args);
 }
 
-
 replInstance.eval = run;
+context.replInstance = replInstance;
 module.exports = context;
--- scope.js
+++ scope.js
@@ -1,7 +1,46 @@
+var plugin = {};
+
 var scope = {
     "a": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    "o": {"keyA": "valueA", "keyB": "valueB", "keyC": "valueC"},
-    "fnc":function(paramA){ return paramA}
+    "o": {
+        "keyA": "valueA",
+        "keyB": "valueB",
+        "keyC": "valueC"
+    },
+    "fnc": function(paramA) {
+        return paramA
+    },
+    plugin: plugin
+}
+
+
+
+var util = require('util');
+scope.out = function(msg) {
+    console.log(util.inspect(msg, true, 4));
+}
+
+scope.exec = function(cmd) {
+    console.log(child_process.execSync(cmd).toString(), true);
+}
+
+scope.initLoopback = function() {
+    plugin.loopback = require('loopback');
+    plugin.app = plugin.loopback();
+
+    plugin.model = plugin.loopback.PersistedModel.extend('CartItem', {
+        tax: {
+            type: Number,
+            default: 0.1
+        },
+        price: Number,
+        item: String,
+        qty: {
+            type: Number,
+            default: 0
+        },
+        cartId: Number
+    });
 }
 
 module.exports = scope;
